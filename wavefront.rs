0c315508fec67b33ed7e2865bf2af89d88ad8416
Robert Norris <rob@eatenbyagrue.org>
2013-04-16 10:22:07 +1000

    initial attempt at drawing something

--- wavefront.rs
+++ wavefront.rs
@@ -1,3 +1,9 @@
+extern mod sdl;
+extern mod opengles;
+
+use opengles::gl2;
+use opengles::gl2::{GLuint, GLint, GLenum};
+
 struct Vector2 {
     x: float,
     y: float,
@@ -49,16 +55,16 @@ impl Vector3 {
 }
 
 struct Vertex {
-    position: uint,
-    texcoord: uint,
-    normal: uint,
+    position: u16,
+    texcoord: u16,
+    normal: u16,
 }
 
 impl Vertex {
-    fn from_str_vec(v: &str) -> Vertex {
+    fn from_str(v: &str) -> Vertex {
         let mut idx = ~[];
         for str::each_split_char(v, '/') |s| {
-            idx.push(match uint::from_str(s) {
+            idx.push(match u16::from_str(s) {
                 Some(i) => i,
                 None => 0,
             });
@@ -66,7 +72,7 @@ impl Vertex {
         Vertex::from_vec(idx)
     }
 
-    fn from_vec(v: &[uint]) -> Vertex {
+    fn from_vec(v: &[u16]) -> Vertex {
         assert!(v.len() == 3);
         Vertex {
             position: v[0],
@@ -84,7 +90,7 @@ struct Face {
 impl Face {
     fn from_str_vec(v: &[~str]) -> Face {
         Face::from_vec(do v.map |s| {
-            Vertex::from_str_vec(*s)
+            Vertex::from_str(*s)
         })
     }
 
@@ -143,9 +149,134 @@ fn load_wavefront(filename: &str) -> Mesh {
     mesh
 }
 
+fn make_buffer<T>(target: GLenum, data: &[T]) -> GLuint {
+    let buffers = gl2::gen_buffers(1);
+    let buffer = *(buffers.head());
+    gl2::bind_buffer(target, buffer);
+    gl2::buffer_data(target, data, gl2::STATIC_DRAW);
+    buffer
+}
+
+fn make_shader(ty: GLenum, filename: &str) -> GLuint {
+    let path = Path(filename);
+    let r = match io::file_reader(&path) {
+        Err(err) => fail!(fmt!("couldn't open %s for read: %s", filename, err)),
+        Ok(r) => r,
+    };
+    let source = ~[r.read_whole_stream()];
+
+    let shader = gl2::create_shader(ty);
+    gl2::shader_source(shader, source);
+    gl2::compile_shader(shader);
+
+    match gl2::get_shader_iv(shader, gl2::COMPILE_STATUS) as bool {
+        false => fail!(fmt!("failed to compile %s: %s", filename, gl2::get_shader_info_log(shader))),
+        true => {}
+    };
+
+    shader
+}
+
+fn make_program(vertex_shader: GLuint, fragment_shader: GLuint) -> GLuint {
+    let program = gl2::create_program();
+    gl2::attach_shader(program, vertex_shader);
+    gl2::attach_shader(program, fragment_shader);
+    gl2::link_program(program);
+
+    match gl2::get_program_iv(program, gl2::LINK_STATUS) as bool {
+        false => fail!(fmt!("failed to link shader program: %s", gl2::get_program_info_log(program))),
+        true => {}
+    };
+
+    program
+}
+
+
+fn check_gl_error() {
+    match gl2::get_error() {
+        gl2::NO_ERROR => {},
+        e => io::println(fmt!("GL error: %s", match e {
+            gl2::INVALID_ENUM                  => ~"INVALID_ENUM",
+            gl2::INVALID_VALUE                 => ~"INVALID_VALUE",
+            gl2::INVALID_OPERATION             => ~"INVALID_OPERATION",
+            gl2::STACK_OVERFLOW                => ~"STACK_OVERFLOW",
+            gl2::STACK_UNDERFLOW               => ~"STACK_UNDERFLOW",
+            gl2::OUT_OF_MEMORY                 => ~"OUT_OF_MEMORY",
+            gl2::INVALID_FRAMEBUFFER_OPERATION => ~"INVALID_FRAMEBUFFER_OPERATION",
+            _ => fmt!("unknown 0x%x", e as uint),
+        }))
+    }
+}
+
+
 fn main() {
     #[main];
 
     let mesh = load_wavefront("ship.obj");
-    io::println(fmt!("%?", mesh));
+
+    do sdl::start {
+        sdl::init([sdl::InitVideo]);
+
+        let info = sdl::video::get_video_info();
+        let (rs, gs, bs) = match info.format.bpp {
+            16      => (5, 6, 5),
+            24 | 32 => (8, 8, 8),
+            _       => fail!(fmt!("invalid pixel depth: %d bpp", info.format.bpp as int))
+        };
+
+        sdl::gl::set_attribute(sdl::gl::RedSize, rs);
+        sdl::gl::set_attribute(sdl::gl::GreenSize, gs);
+        sdl::gl::set_attribute(sdl::gl::BlueSize, bs);
+        sdl::gl::set_attribute(sdl::gl::DepthSize, 24);
+        sdl::gl::set_attribute(sdl::gl::DoubleBuffer, 1);
+        sdl::gl::set_attribute(sdl::gl::SwapControl, 1);
+
+        match sdl::video::set_video_mode(1440, 900, info.format.bpp as int, [], [sdl::video::OpenGL]) {
+            Ok(_)    => {},
+            Err(err) => fail!(fmt!("failed to set video mode: %s", err))
+        };
+
+        sdl::wm::set_caption("wavefront", "wavefront");
+
+        let program = make_program(
+            make_shader(gl2::VERTEX_SHADER, "wavefront.v.glsl"),
+            make_shader(gl2::FRAGMENT_SHADER, "wavefront.f.glsl")
+        );
+        let attr_position = gl2::get_attrib_location(program, ~"position");
+
+        let vertex_buffer = make_buffer(gl2::ARRAY_BUFFER,
+            vec::concat(do mesh.vertices.map |v| { ~[v.x,v.y,v.z] }));
+        let element_buffer = make_buffer(gl2::ELEMENT_ARRAY_BUFFER,
+            vec::concat(do mesh.faces.map |f| { ~[f.vertices[0].position, f.vertices[1].position, f.vertices[2].position] }));
+
+        loop main: {
+            loop event: {
+                match sdl::event::poll_event() {
+                    sdl::event::QuitEvent => break main,
+                    sdl::event::NoEvent   => break event,
+                    sdl::event::KeyEvent(sdl::event::EscapeKey, true, _, _) => break main,
+                    _                     => {}
+                }
+            }
+
+            gl2::use_program(program);
+
+            gl2::bind_buffer(gl2::ARRAY_BUFFER, vertex_buffer);
+            gl2::vertex_attrib_pointer_f32(attr_position as GLuint, 3, false, 0, 0);
+            gl2::enable_vertex_attrib_array(attr_position as GLuint);
+
+            gl2::bind_buffer(gl2::ELEMENT_ARRAY_BUFFER, element_buffer);
+
+            gl2::draw_elements(gl2::TRIANGLES, (mesh.faces.len()*3) as GLint, gl2::UNSIGNED_SHORT, None);
+
+            gl2::bind_buffer(gl2::ARRAY_BUFFER, 0);
+            gl2::bind_buffer(gl2::ELEMENT_ARRAY_BUFFER, 0);
+
+            sdl::gl::swap_buffers();
+
+            check_gl_error();
+        }
+
+        sdl::quit();
+    }
 }
